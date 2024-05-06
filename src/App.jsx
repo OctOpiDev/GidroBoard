2cdb4dae64a1e0c83c88da7fb7febb408c4fc292
OctOpi <64381167+OctOpiDev@users.noreply.github.com>
2024-05-06 23:41:05 +0300

    del camerapage

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import LayoutApp from './components/layout/LayoutApp'
 import Userpage from './pages/Userpage'
 import LoginPage from './pages/auth/LoginPage'
 import DocsPage from './pages/auth/docs/DocsPage'
-import CameraPage from './pages/Camerapage'
-// import CameraPage from './pages/CameraPage'
+
+
 
 
 
@@ -21,7 +21,6 @@ function App() {
   <Route path='/login' element={<LoginPage />} />
     <Route path='/' element={<LayoutApp />}>
       <Route index element={<Homepage />} />
-      {/* <Route path='/videos' element={<CameraPage />} /> */}
       <Route path='/users' element={<Userpage />} />
       <Route path='/docs' element={<DocsPage />} />
     </Route>
